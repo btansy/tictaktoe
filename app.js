1d0590c5f4c61b151571947a0d1e621531357876
Brian Tansy <btansy@gmail.com>
2020-01-13 16:10:21 -0800

    done with min

--- app.js
+++ app.js
@@ -28,13 +28,13 @@ var render = function() {
     var y = i;
     for (var j = 1; j < model[i].length; j ++) {
       var x = j;
-      if (model[x][y] === 1) {
+      if (model[y][x] === 1) {
        document.getElementById(x + 'x' + y).innerHTML = '<h3>X</h3>';
       }
-      if (model[x][y] === 2) {
+      if (model[y][x] === 2) {
         document.getElementById(x + 'x' + y).innerHTML = '<h3>O</h3>';
       }
-      if (model[x][y] === 0) {
+      if (model[y][x] === 0) {
         document.getElementById(x + 'x' + y).innerHTML = '';
       }
     }
@@ -44,36 +44,34 @@ var render = function() {
   This is how the player gives input to the game.
 */
 var playerMove = function(x,y) {
+  if ((victory || compVictory) === true) {
+      return;
+  }
   console.log('player move start');
-  if (model[x][y] === (1 || 2)) {
+  if (model[y][x] === (1 || 2)) {
     alert("That is an illegal move. Please try again.");
   }
-  if (model[x][y] === 0) {
-    model[x][y] = 1;
+  if (model[y][x] === 0) {
+    model[y][x] = 1;
     var index = openSpots.indexOf(x + 'x' + y);
     openSpots.splice(index, 1);
     turnCount++;
     render();
     checkVictory();
     if (victory === true) {
-        alert("You win!");
-        resetBoard();
-        render();
-        return;
+        document.getElementById('title').innerHTML=
+            'You Win! <button onclick=resetBoard()>Try Again</button>';
     }
     if (openSpots.length === 0) {
-        alert("You Tie!");
-        resetBoard();
-        render();
-        return;
+        document.getElementById('title').innerHTML= 
+            'You Tie! <button onclick=resetBoard()>Try Again</button>';
     }
     computerMove(model);
     render();
     checkCompVictory();
     if (compVictory === true) {
-        alert("You Lose!");
-        resetBoard();
-        render();
+        document.getElementById('title').innerHTML=
+            'You Lose! <button onclick=resetBoard()>Try Again</button>';
     }
   }
 };
@@ -82,9 +80,10 @@ var computerMove = function (model) {
   if (openSpots.length > 0) {
     var spotForCompMove = openSpots[Math.floor(Math.random() * Math.floor(openSpots.length - 1))];
     var newCoords = spotForCompMove.split('x');
-    model[newCoords[0]][newCoords[1]] = 2;
+    model[newCoords[1]][newCoords[0]] = 2;
     var index = openSpots.indexOf(spotForCompMove);
     openSpots.splice(index, 1);
+    render();
   }
 };
 
@@ -99,6 +98,8 @@ var resetBoard = function() {
   ];
   openSpots = ['1x1','1x2','1x3','2x1','2x2','2x3','3x1','3x2','3x3'];
   turnCount = 0;
+  document.getElementById('title').innerHTML= 'This is Tic Tac Toe';
+  render();
 };
 
 var checkCompVictory = function() {
@@ -108,8 +109,8 @@ var checkCompVictory = function() {
   if ((model[1][1] == 2) && (model[2][1] == 2) && (model[3][1] == 2)) {compVictory = true};
   if ((model[1][2] == 2) && (model[2][2] == 2) && (model[3][2] == 2)) {compVictory = true};
   if ((model[1][3] == 2) && (model[2][3] == 2) && (model[3][3] == 2)) {compVictory = true};
-//   if ((model[1][1] == 2) && (model[2][2] == 2) && (model[3][3] == 2)) {compVictory = true};
-//   if ((model[1][3] == 2) && (model[2][2] == 2) && (model[3][1] == 2)) {compVictory = true};
+  if ((model[1][1] == 2) && (model[2][2] == 2) && (model[3][3] == 2)) {compVictory = true};
+  if ((model[1][3] == 2) && (model[2][2] == 2) && (model[3][1] == 2)) {compVictory = true};
 };
 
 var checkVictory = function() {
@@ -119,6 +120,6 @@ var checkVictory = function() {
   if ((model[1][1] == 1) && (model[2][1] == 1) && (model[3][1] == 1)) {victory = true};
   if ((model[1][2] == 1) && (model[2][2] == 1) && (model[3][2] == 1)) {victory = true};
   if ((model[1][3] == 1) && (model[2][3] == 1) && (model[3][3] == 1)) {victory = true};
-//   if ((model[1][1] == 1) && (model[2][2] == 1) && (model[3][3] == 1)) {compVictory = true};
-//   if ((model[1][3] == 1) && (model[2][2] == 1) && (model[3][1] == 1)) {compVictory = true};
+  if ((model[1][1] == 1) && (model[2][2] == 1) && (model[3][3] == 1)) {victory = true};
+  if ((model[1][3] == 1) && (model[2][2] == 1) && (model[3][1] == 1)) {victory = true};
 };
